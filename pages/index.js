d6afc987437dfffe80d7a499ec810f83ff0a54e8
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-05-09 17:08:30 +0330

    feat ( flowbite) : add flowbite and flowbite react to tailwind configure

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <div className="h-screen ">
+    <div className="h-screen flex items-center gap-2 justify-center  w-full bg-slate-400">
       <h2>ali</h2>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,5 +9,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
