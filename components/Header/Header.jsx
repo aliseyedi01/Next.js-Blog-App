83991278d6aaaed60346203210f615713cee8b4f
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-05-11 00:13:05 +0330

    feat ( header ) : update header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,75 @@
 import React from "react";
+import Link from "next/link";
+import { BsFillEnvelopeAtFill } from "react-icons/bs";
+import { AiFillFileText, AiFillHome } from "react-icons/ai";
+import { HiUserGroup } from "react-icons/hi";
+import { useRouter } from "next/router";
+
+const Items = [
+  {
+    name: "Home",
+    path: "/home",
+    icon: <AiFillHome />,
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+    icon: <AiFillFileText />,
+  },
+  {
+    name: "About",
+    path: "/about",
+    icon: <HiUserGroup />,
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+    icon: <BsFillEnvelopeAtFill />,
+  },
+];
 
 export default function Header() {
+  const router = useRouter();
+  const activeRouter = router.asPath;
+  console.log(router.asPath);
+
   return (
-    <>
-      <p>ali</p>
-    </>
+    <div className="bg-sky-500 w-full  flex justify-between  items-center  h-14 px-3">
+      <div className="h-full flex gap-2 items-center">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1809/1809216.png"
+          alt="name app"
+          className="h-11"
+        />
+        <p className="font-mono text-xl uppercase font-bold">Blog</p>
+      </div>
+
+      <div className="h-full flex  ">
+        <ul className="flex gap-2 font-serif  font-bold capitalize items-center">
+          {Items.map((item, i) => (
+            <Link rel="stylesheet" href={item.path}>
+              <li
+                key={i}
+                className={`cursor-pointer hover:bg-green-400 flex rounded-md items-center gap-1  px-2 py-1  ${
+                  item.path === activeRouter ? "text-white" : ""
+                }`}
+              >
+                {item.icon}
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="h-full flex justify-between gap-3 items-center ">
+        <span className="pi pi-bell text-2xl "></span>
+        <img
+          className="h-9 rounded-full"
+          src="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-male-avatar-simple-cartoon-design-png-image_1934458.jpg"
+          alt="user"
+        />
+      </div>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,8 @@ import { Button } from "primereact/button";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center  gap-2 bg-slate-400">
-      <Button label="Submit" />
-      <Button icon="pi pi-check" />
-      <Button label="Submit" icon="pi pi-check" />
-      <Button label="Submit" icon="pi pi-check" iconPos="right" />
+    <div className="flex w-full h-screen justify-center item bg-slate-400">
+      <h2>ali</h2>
     </div>
   );
 }
